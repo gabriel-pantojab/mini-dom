c8daedf4881dbecb5bc324eec9f1d608b7af9acd
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-10-01 09:36:15 -0300

    refactor: add diffProps to handle property/attribute assignment in applyProperties

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,11 +4,21 @@ function applyClass(element, classes) {
 	});
 }
 
+function diffProps(element, prop, propValue) {
+	if (prop in element) {
+		element[prop] = propValue;
+	} else {
+		element.setAttribute(prop, propValue);
+	}
+}
+
 export function applyProperties(element, props) {
 	for (let prop in props) {
 		if (prop === 'className') {
 			const classes = props.className.split(' ');
 			applyClass(element, classes);
-		} else element.setAttribute(prop, props[prop]);
+		} else {
+			diffProps(element, prop, props[prop]);
+		}
 	}
 }
